2e7c21284f7bc38409a975045e917a9d91743ce3
Roberto Gomez <rob.77@live.com>
2020-09-23 17:33:17 -0500

    Data Types completed

--- Tutorial/DataTypes/Program.cs
+++ Tutorial/DataTypes/Program.cs
@@ -6,8 +6,39 @@ namespace DataTypes
    {
       static void Main(string[] args)
       {
-         int myNum = 5;
+
          Console.WriteLine("Data Types");
+
+
+         int myNum = 5; //  Integer
+         double myDoubleNum = 5.99D; // floating point number
+         char myLetter = 'D'; // Character
+         bool myBool = true; // Boolean
+         string myText = "Hello"; // String
+
+         int myNumber = 10000;
+         Console.WriteLine(myNumber);
+
+         long myNum2 = 15000000L;
+         Console.WriteLine(myNum2);
+
+         float myNum3 = 5.75F;
+         Console.WriteLine(myNum3);
+
+         double myNum4 = 19.99D;
+         Console.WriteLine(myNum4);
+
+         bool isCSharpFun = true;
+         bool isFishTasty = false;
+         Console.WriteLine(isCSharpFun);
+
+         char myGrade = 'B';
+         Console.WriteLine(myGrade);
+
+         string greeting = "Hello World";
+         Console.WriteLine(greeting);
+
       }
    }
 }
+
