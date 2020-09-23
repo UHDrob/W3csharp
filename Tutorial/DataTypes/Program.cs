7f0c52c9a72864588ea50618fb3567bc76767218
Roberto Gomez <rob.77@live.com>
2020-09-23 16:15:02 -0500

    Data Types 003

---
+++ Tutorial/DataTypes/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DataTypes
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         int myNum = 5;
+         Console.WriteLine("Data Types");
+      }
+   }
+}
