6cde1a6d1bedabef6ea1fe5ac47663d1712a2d23
Roberto Gomez <rob.77@live.com>
2020-09-23 17:52:05 -0500

    Type Casting completed

---
+++ Tutorial/TypeCasting/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TypeCasting
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Type Casting!");
+         int myInt = 9;
+         double myDouble = myInt;
+
+         Console.WriteLine(myInt);
+         Console.WriteLine(myDouble);
+
+         double myDouble2 = 9.78;
+         int myInt2 = (int) myDouble;
+         
+         Console.WriteLine(myDouble2);
+         Console.WriteLine(myInt);
+
+         int myInt3 = 10;
+         double myDouble3 = 5.25;
+         bool myBool = true;
+
+         Console.WriteLine(Convert.ToString(myInt3));
+         Console.WriteLine(Convert.ToDouble(myInt3));
+         Console.WriteLine(Convert.ToInt32(myDouble));
+         Console.WriteLine(Convert.ToString(myBool));
+      }
+   }
+}
