e4f291018d72eccff8869f65275e03173135f7eb
Roberto Gomez <rob.77@live.com>
2020-09-23 13:44:29 -0500

    Comments

---
+++ Tutorial/Comments/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+ *  UHCL
+ */
+
+
+using System;
+
+namespace Comments
+{
+   class Program
+   {
+      // This is a comment
+
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello Comments!");  // THis is a comment
+      }
+   }
+}
