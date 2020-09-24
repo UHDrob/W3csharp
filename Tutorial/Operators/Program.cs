79ebed33d7b903bf7ff6f95e6eb59599620667e4
Roberto Gomez <rob.77@live.com>
2020-09-23 20:21:35 -0500

    math

--- Tutorial/Operators/Program.cs
+++ Tutorial/Operators/Program.cs
@@ -6,7 +6,18 @@ namespace Operators
    {
       static void Main(string[] args)
       {
-         Console.WriteLine("Hello World!");
+         Console.WriteLine("Operators!");
+
+         int x = 100 + 50;
+
+         int sum1 = 100 + 50;
+         int sum2 = sum1 + 250;
+         int sum3 = sum2 + sum2;
+
+         int x1 = 10;
+         x1 +=5;
+
+
       }
    }
 }
