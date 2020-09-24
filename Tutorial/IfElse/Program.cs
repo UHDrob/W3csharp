e3b5a9b4964938e2c5b2c9af61cff94d26f57f71
Roberto Gomez <rob.77@live.com>
2020-09-23 22:36:06 -0500

    Switch

--- Tutorial/IfElse/Program.cs
+++ Tutorial/IfElse/Program.cs
@@ -7,6 +7,39 @@ namespace IfElse
       static void Main(string[] args)
       {
          Console.WriteLine("If Else!");
+
+         if (20>18)
+         {
+            Console.WriteLine("20 is greater than 18");
+         }
+
+         int x = 20;
+         int y = 18;
+         if (x > y)
+         {
+            Console.WriteLine("x is greater than y");
+
+         }
+
+         int time = 20;
+         if (time < 10)
+         {
+            Console.WriteLine("Good Morning.");
+         }
+         else if ( time< 20)
+         {
+            Console.WriteLine("Good day");
+         }
+         else
+         {
+            Console.WriteLine("Good evening");
+         }
+
+         // Outputs "Good Evening"
+
+         int time2 = 20;
+         string result = (time < 18) ? "Good day" : "Good evening";
+         Console.WriteLine(result);
       }
    }
 }
