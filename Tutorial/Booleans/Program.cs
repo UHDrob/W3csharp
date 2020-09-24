2594adb280f223e7ed029c88b8e08ed0da729c5b
Roberto Gomez <rob.77@live.com>
2020-09-23 20:29:57 -0500

    If Else

--- Tutorial/Booleans/Program.cs
+++ Tutorial/Booleans/Program.cs
@@ -7,6 +7,21 @@ namespace Booleans
       static void Main(string[] args)
       {
          Console.WriteLine("Booleans World!");
+         bool isCSharFun = true;
+         bool isFishTasty = false;
+         Console.WriteLine(isCSharFun);
+         Console.WriteLine(isFishTasty);
+
+         int x = 10;
+         int y = 9;
+         Console.WriteLine(x > y);
+         Console.WriteLine( 10> 9);
+
+         Console.WriteLine(x == 10);
+
+         Console.WriteLine(10 == 15);
+
+
       }
    }
 }
---
+++ Tutorial/IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IfElse
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("If Else!");
+      }
+   }
+}
